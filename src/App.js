82865dde9817210c1b7d6d52fb5251ba7e10b065
naser23 <naserabdulkadir0@gmail.com>
2022-04-17 13:10:25 -0400

    All of the pages are in the repo now. I will work on these and eventually create a profile page on my own.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Bookmarked from "./pages/Bookmarked/Bookmarked";
+import Movies from "./pages/Movies/Movies";
+import TvSeries from "./pages/TV-Series/TvSeries";
+import LogIn from "./pages/LogIn/LogIn";
+import SignUp from "./pages/SignUp/SignUp";
+
 function App() {
   return (
-    <div className="App">
-      <h1>My App</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/movies" element={<Movies />}></Route>
+        <Route path="/tv-series" element={<TvSeries />}></Route>
+        <Route path="/bookmarked" element={<Bookmarked />}></Route>
+        <Route path="/log-in" element={<LogIn />}></Route>
+        <Route path="/sign-up" element={<SignUp />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Bookmarked/Bookmarked.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Bookmarked() {
+  return <div>Bookmarked</div>;
+}
+
+export default Bookmarked;
