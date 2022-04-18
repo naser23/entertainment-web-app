33012dd948da38578ac0d207fe463a5d79d5d021
naser23 <naserabdulkadir0@gmail.com>
2022-04-18 14:54:59 -0400

    Working on SearchBar component. Trying to get it positioned correctly on larger screens. I'm considering using grid as I think that might solve the problem.

--- src/components/NavbarRoute.js
+++ src/components/NavbarRoute.js
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar/Navbar";
+import SearchBar from "./SearchBar/SearchBar";
 
 const NavbarRoute = () => {
   return (
     <>
       <Navbar />
+      <SearchBar />
       <Outlet />
     </>
   );
---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import SearchIcon from "../../assets/icon-search.svg";
+
+function SearchBar() {
+  function onSubmit(e) {
+    e.preventDefault();
+  }
+
+  return (
+    <form onSubmit={onSubmit}>
+      <div className="searchContainer">
+        <img className="searchIcon" src={SearchIcon} alt="search icon" />
+        <input
+          className="searchBar"
+          type="text"
+          placeholder="Search for movies or TV shows"
+        />
+      </div>
+    </form>
+  );
+}
+
+export default SearchBar;
