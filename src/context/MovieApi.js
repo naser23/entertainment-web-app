004327c2805be6fc0fa3c03ad5a38ccc0dc4da4b
naser23 <naserabdulkadir0@gmail.com>
2022-04-21 10:14:51 -0400

    I might not use the MovieDb api after all. I'm still deciding but just in case I set up the fetch request with the data.json file in the mean time.

--- src/context/MovieApi.js
+++ src/context/MovieApi.js
@@ -3,33 +3,39 @@ import { createContext, useState, useEffect } from "react";
 const MovieApiContext = createContext();
 
 export function MovieApiProvider({ children }) {
-  const options = {
-    method: "GET",
+  const [data, setData] = useState();
+  // const options = {
+  //   method: "GET",
+  //   headers: {
+  //     "X-RapidAPI-Host": "mdblist.p.rapidapi.com",
+  //     "X-RapidAPI-Key": "2fd2c0727dmsh5b922ccc270d30dp14ee5bjsna1a90ecec93c",
+  //   },
+  // };
+
+  const settings = {
     headers: {
-      "X-RapidAPI-Host": "mdblist.p.rapidapi.com",
-      "X-RapidAPI-Key": "2fd2c0727dmsh5b922ccc270d30dp14ee5bjsna1a90ecec93c",
+      "Content-Type": "application/json",
+      Accept: "application/json",
     },
   };
 
   async function getData() {
     try {
-      const response = await fetch(
-        "https://mdblist.p.rapidapi.com/?s=spiderman",
-        options
-      );
-      const json = response.json();
-      console.log(json);
+      const response = await fetch("data.json", settings);
+      const result = await response.json();
+      console.log(result);
     } catch (error) {
       console.log(error);
     }
   }
 
+  // getData();
+
+  // console.log(data);
   //   fetch("https://mdblist.p.rapidapi.com/?s=jaws", options)
   //     .then((response) => response.json())
   //     .then((response) => console.log(response))
   //     .catch((err) => console.error(err));
-
-  const [data, setData] = useState();
 
   return (
     <MovieApiContext.Provider value={data}>{children}</MovieApiContext.Provider>
