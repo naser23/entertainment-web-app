45ca77f69ec2d9796f2f3190cd2fe7501101fe12
naser23 <naserabdulkadir0@gmail.com>
2022-06-13 13:50:11 -0400

    Still Working on movieDetails page. Have to design it as I go.

--- src/pages/MovieDetails/MovieDetails.js
+++ src/pages/MovieDetails/MovieDetails.js
@@ -35,20 +35,37 @@ function MovieDetails() {
         <div className="loading"></div>
       )}
       <section className="infoSection">
+        {/* // header and tagline */}
         <h1 className="mediaDetailsTitle">
+          {/* tell whether it's a movie or a tv show */}
           {data && (data.title ? data.title : data.name)}
         </h1>
-        <div className="releaseInfo">
+        {data && <p className="tagline">"{data.tagline}"</p>}
+
+        <div className="topInfo">
           {data &&
             (type == "movie" ? (
               // if user clicks on movie, display this text.
-              <p>Release Date: {data.release_date}</p>
+              <p>
+                Release Date: {data.release_date ? data.release_date : "N/A"}
+              </p>
             ) : (
               // else if tv show, display this text
               <>
-                <p>First episode aired on: {data.first_air_date}.</p>
-                <p>Latest episode aired on: {data.last_air_date}</p>
+                <p>
+                  {data.first_air_date
+                    ? data.first_air_date.slice(0, 4)
+                    : "N/A"}
+                </p>
               </>
+            ))}
+
+          <div className="smallCircle"></div>
+          {data &&
+            data.genres.map((genre) => (
+              <p className="genreNames" key={genre.id}>
+                {genre.name}
+              </p>
             ))}
         </div>
       </section>
